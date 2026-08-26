8a8a42e9c0ff92ea2fb64e1a1b85c08b201e7717
Sharath Bk <bksharath3@gmail.com>
2026-08-27 05:29:47 +0530

    Add 11hello practice

---
+++ 11hello.py
@@ -0,0 +1,30 @@
+#while loop
+
+
+i=1
+while i<=20:
+    print(i)
+    if i%2!=0:
+        print("odd number")
+    else:
+        print("even number")
+    i+=1
+
+i=1
+while i<=20:
+    print(1)
+    i+=1
+
+    i=1
+while i<=5:
+    print("sharath")
+    i+=1
+
+i=1
+while i <=10:
+    j=1
+    while j<=i:
+        print("sharath" , end=" ")
+        j+=1
+    print()
+    i+=1
