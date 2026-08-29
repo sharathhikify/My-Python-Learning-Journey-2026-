71f61f9df58229496954522687b17e4f23c52f4d
Sharath Bk <bksharath3@gmail.com>
2026-08-29 05:44:21 +0530

    Add 13hello practice

---
+++ 13hello.py
@@ -0,0 +1,11 @@
+name="sharath"
+for i, names in enumerate(name):
+    if i%2==0:
+        print(f"{names} " * 2)
+    else:
+        print(f"{names}")
+
+l=[13, 24, 35, 46, 57, 68, 79, 80]
+for index, num in enumerate(l):
+    print(f"{num} is in {index}th index")
+   
